ecdeb7dc0ee6dc9dd782a377842d426503a1985e
elis-11 <eliza.arzanukaeva@gmail.com>
2022-10-19 12:24:29 +0200

    Tailwind

--- react-training/src/components/State.jsx
+++ react-training/src/components/State.jsx
@@ -1,22 +1,29 @@
-import { useState } from 'react'
-import { TodoList } from '../todo/TodoList'
+import { useState } from "react";
+import { TodoList } from "../todo/TodoList";
 
 export const State = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, title: "First Todo", completed: false },
+    { id: 2, title: "Second Todo", completed: false },
+  ]);
 
-const [todos, setTodos] = useState([
-  {id: 1, title: 'First Todo', completed: false},
-  {id: 2, title: 'Second Todo', completed: false},
-])
-
+  
   return (
-    <div>
-      <h2>Todo App</h2>
-      <div className='w-full h-20 border-b-2 border-gray-400 bg-slate-200'>
-        <input type='text' className=''/>
-        <label>Todo name</label>
-      </div>
+    <div className="w-full flex column justify-center">
+      <div className="w-">
+        <h2 className="font-semibold text-lg ">Todo App</h2>
+        <div className="w-full h-20 border-b-2 ">
+          <input
+            autoFocus
+            type="text"
+            placeholder="Todo name..."
+            className="w-96 h-5 file:mx-8 ml-9 mt-10 font-bold"
+          />
+          {/* <label>Todo name</label> */}
+        </div>
 
-      <TodoList todos={todos}/>
+        <TodoList todos={todos} />
+      </div>
     </div>
-  )
-}
+  );
+};
--- react-training/src/todo/TodoItem.jsx
+++ react-training/src/todo/TodoItem.jsx
@@ -3,10 +3,10 @@ import {BsTrash} from 'react-icons/bs'
 export const TodoItem = ({title, id, completed}) => {
   return (
     <li>
-        <label className='flex justify-between items-center w-100 h-8 bg-red-100 my-1'>
-            <input type="checkbox" defaultChecked={false}/>
+        <label className='flex justify-between items-center w-100 h-12 bg-gray-100 my-1 px-5'>
+            <input type="checkbox" defaultChecked={false} className='cursor-pointer'/>
             <span>{title}</span>
-            <BsTrash className='text-gray-500'/>
+            <BsTrash className=' text-gray-500 hover:text-gray-400 cursor-pointer'/>
         </label>
     </li>
   )
