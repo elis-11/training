1d5de28ad086499749414ad5243cda6a3a8c08c3
elis-11 <eliza.arzanukaeva@gmail.com>
2022-10-23 13:08:58 +0200

    Update

--- hooks/src/memo/IsFive.jsx
+++ hooks/src/memo/IsFive.jsx
@@ -2,21 +2,30 @@ import React, { useMemo } from "react";
 
 let renderCount = 0;
 
-export default React.memo(function IsFive ({ value }) {
-  console.warn(`isFive render: ${++renderCount}`);
+export default React.memo(
+  function IsFive({ value }) {
+    console.warn(`isFive render: ${++renderCount}`);
 
-  // const getResult = () => {       //without useMemo
-  //   let i = 0;
-  //   while (i < 60000000) i++;
-  //   return value === 5 ? " It's five  :)" : "  It isn't five :(";
-  // };
+    // const getResult = () => {       //without useMemo
+    //   let i = 0;
+    //   while (i < 60000000) i++;
+    //   return value === 5 ? " It's five  :)" : "  It isn't five :(";
+    // };
 
-  const getResult = useMemo(() => {
-    let i = 0;
-    while (i < 60000000) i++;
-    return value === 5 ? " It's five  :)" : "  It isn't five :(";
-  }, [value]);
+    const getResult = useMemo(() => {
+      let i = 0;
+      while (i < 60000000) i++;
+      return value === 5 ? " It's five  :)" : "  It isn't five :(";
+    }, [value]);
 
-  // return <h3>{getResult()}</h3>;  //without useMemo
-  return <h3>{getResult}</h3>;
-});
+    // return <h3>{getResult()}</h3>;  //without useMemo
+    return <h3>{getResult}</h3>;
+  },
+  (prevProps, nextProps) => {
+    if (nextProps.value === 5) {  // if value === 5
+      return false;
+    } else {
+      return true;
+    }
+  }
+);
