b8a84d0e7bfc2aaf192ab7550001b71783a6fa98
elis-11 <eliza.arzanukaeva@gmail.com>
2023-03-05 20:40:44 +0100

    Job-Interview - 1

---
+++ interview1.js
@@ -0,0 +1,14 @@
+const myName = "John Doe"
+
+function sayName() {
+    console.log("myName: ", myName);
+    return myName;
+}
+// sayName();
+console.log(sayName());
+
+const obj = {name: "John Doe", age: 30}
+const obj2 = new Object()
+
+console.log("obj:", obj);
+console.log("obj2:", obj2);
